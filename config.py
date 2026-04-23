62885750819dba5cd2a1d52e4ab5ffe0c19ce26b
OmarGaber <omar.gaber323@gmail.com>
2026-04-23 23:06:21 +0200

    fix: fix summary table, data validation

--- config.py
+++ config.py
@@ -1,34 +1,34 @@
 from algorithms import alpha_beta, minimax, is_conflict_avoided
 from state import deviation, State, Aircraft
+from tabulate import tabulate
+from copy import deepcopy
 container = []
 class Config:
 
     results = {
-    'conflicts' : 0,
     'decision_sequence_first' : [],
     'decision_sequence_second' : [],
-    'is_conflict_avoided' : '',
+    'deviation_first':0,
+    'deviation_second':0,
     'nodes_evaluated' : [],
+    'is_conflict_avoided' : '',
     'value' : 0,
     'nodes_evaluated_minimax' : [],
-    'deviation_first':0,
-    'deviation_second':0
+    'conflicts' : 0,
+    'num_of_alphabeta_nodes':0,
+    'num_of_minimax_nodes':0,
     }
 
     def __init__(self, a_start,a_target, b_start , b_target,maximising = True,depth=6,alpha=float('-inf'),beta=float('inf')):
-        def validate (val):
-            if val < 0: return 0
-            if val > 10: return 10
-            return val
-        #add data validation
-        self.a_start = a_start
-        self.a_target = a_target
-        self.b_start = b_start
-        self.b_target = b_target
+        
+        self.a_start  = self.validate(a_start,  1, 10, "The start of first aircraft")
+        self.a_target = self.validate(a_target, 1, 10, "The target of first aircraft")
+        self.b_start  = self.validate(b_start,  1, 10, "The start of second aircraft")
+        self.b_target = self.validate(b_target, 1, 10, "The target of first aircraft")
+        self.depth    = self.validate(depth,    1,  6, "The depth")
         self.maximising = maximising
-        self.depth = depth
         self.alpha = alpha
-        self.beta = beta
+        self.beta  = beta
         self.A = Aircraft(self.a_start,self.a_target)
         self.B = Aircraft(self.b_start,self.b_target)
         current_state = State(self.A, self.B)
@@ -43,6 +43,17 @@ class Config:
         self.save_results(self.results)
         self.reset_results(self.results)
 
+    def validate(self,val, min_val, max_val, name):
+            if val < min_val:
+                print(f"Warning: {name} = {val} is below minimum ({min_val}), set to {min_val}.")
+                return min_val
+            if val > max_val:
+                print(f"Warning: {name} = {val} exceeds maximum ({max_val}), set to {max_val}.")
+                return max_val
+            return val
+
+    def save_results(self,results):
+        container.append(deepcopy(results))
 
     def reset_results(self,results):  
         results['conflicts'] = 0
@@ -55,8 +66,7 @@ class Config:
         results['deviation_first'] = 0
         results['deviation_second'] = 0
     
-    def save_results(self,results):
-        container.append(self.results)
+    
     
     def outcomes(self,results):
         print(f"Decision sequence for the first plane: {results['decision_sequence_first']}")
@@ -64,58 +74,79 @@ class Config:
         print(f"Is conflict avoided: {results['is_conflict_avoided']}")
         print(f"Deviation for the first plane: {results['deviation_first']}")
         print(f"Deviation for the second plane: {results['deviation_second']}")
-        #Organize the output in a table format
-        print("\n" + "="*70)
-        print(f"{'Nodes evaluated alphabeta':<35} | {'Nodes evaluated minimax':<35}")
-        print("-" * 70)
-        #transfer the list of nodes evaluated to string and print only the first 33 characters followed by '...' to indicate that there are more nodes
         str_ab = str(results['nodes_evaluated'])
         str_mm = str(results['nodes_evaluated_minimax'])
-        print(f"{str_ab[:33]+'..':<35} | {str_mm[:33]+'..':<35}")
-        #print the lengthes of the nodes evaluated for both algorithms
-        len_ab = len(results['nodes_evaluated'])
-        len_mm = len(results['nodes_evaluated_minimax'])
-        print(f"{'Count: ' + str(len_ab):<35} | {'Count: ' + str(len_mm):<35}")
-        print("="*70 + "\n")
-    
-def summary_table():
-    #Side titles
-    row_names = [
-        "Path of A",
-        "Path of B",
-        "Deviation of A",
-        "Deviation of B",
-        "Len(Nodes evaluated alphabeta)",
-        "Is conflict avoided"
-    ]
-    #Count the elements which add to each config in the container
-    num_attributes = 8
-    col_width = 25
-    header_width = 30
-    # Calculate the number of the configs which in the container
-    num_configs = len(container) 
-    # Print the header
-    header_row = f"{'Attribute':<{header_width}}"
-    for i in range(num_configs):
-        header_row += f"| {'Config ' + str(i+1):<{col_width}}"
-    print("\n" + "=" * len(header_row))
-    print(header_row)
-    print("-" * len(header_row))
-    # print rows via pull the values with index
-    for i, name in enumerate(row_names):
-        row_str = f"{name:<{header_width}}"
-        for c_idx in range(num_configs):
-            start_pos = c_idx * num_attributes
-    #Determine the location of the value based on its order in results
-    if name == "Path of A": val = container[start_pos + 1]
-    elif name == "Path of B": val = container[start_pos + 2]
-    elif name == "Deviation of A": val = container[start_pos + 8]
-    elif name == "Deviation of B": val = container[start_pos + 7]
-    elif name == "Len(Nodes evaluated alphabeta)": val = len(container[start_pos + 4])
-    elif name == "Is conflict avoided": val = container[start_pos + 3]
-    
-    row_str += f"| {str(val)[:20]:<{col_width}}"
-    print(row_str)
+        len_ab = results['num_of_alphabeta_nodes']=len(results['nodes_evaluated'])
+        len_mm = results['num_of_minimax_nodes']=len(results['nodes_evaluated_minimax'])
+        rows = [
+            ["Nodes", str_ab[:33] + '..', str_mm[:33] + '..'],
+            ["Count", len_ab, len_mm],
+        ]
+        headers = ["", "Nodes evaluated alphabeta", "Nodes evaluated minimax"]
+        print(tabulate(rows, headers=headers, tablefmt="rounded_grid"))
 
-    print("=" * len(header_row) + "\n")
+def summary_table():
+    field_map = {
+    'decision_sequence_first'  : 'Path of A',
+    'decision_sequence_second' : 'Path of B',
+    'deviation_first'          : 'Deviation of A',
+    'deviation_second'         : 'Deviation of B',
+    'num_of_alphabeta_nodes'   : 'number of alphabeta nodes',
+    'num_of_minimax_nodes'     : 'number of minimax nodes',
+    'is_conflict_avoided'      : 'Is conflict avoided',
+    }
+
+    rows = [
+        [label] + [c[key] for c in container]
+        for key, label in field_map.items()
+    ]
+
+    headers = ["Field"] + [f"config[{i+1}]" for i in range(len(container))]
+
+    print(tabulate(rows, headers=headers, tablefmt="rounded_grid"))
+# def summary_table():
+#     #Side titles
+#     row_names = [
+#         "Path of A",
+#         "Path of B",
+#         "Deviation of A",
+#         "Deviation of B",
+#         "Len(Nodes evaluated alphabeta)",
+#         "Is conflict avoided"
+#     ]
+#     #Count the elements which add to each config in the container
+#     num_attributes = 8
+#     col_width = 25
+#     header_width = 30
+#     # Calculate the number of the configs which in the container
+#     num_configs = len(container) 
+#     # Print the header
+#     header_row = f"{'Attribute':<{header_width}}"
+#     for i in range(num_configs):
+#         header_row += f"| {'Config ' + str(i+1):<{col_width}}"
+#     print("\n" + "=" * len(header_row))
+#     print(header_row)
+#     print("-" * len(header_row))
+#     # print rows via pull the values with index
+#     i = 0
+#     for name in row_names:
+#         if i == len(container):
+#                 break
+#         row_str = f"{name:<{header_width}}"
+#         for c_idx in range(num_configs):
+            
+#             #Determine the location of the value based on its order in results
+#             if name == "Path of A": val = container[i]['decision_sequence_first']
+#             elif name == "Path of B": val = container[i]['decision_sequence_second']
+#             elif name == "Deviation of A": val = container[i]['deviation_first']
+#             elif name == "Deviation of B": val = container[i]['deviation_second']
+#             elif name == "Len(Nodes evaluated alphabeta)": val = len(container[i]['nodes_evaluated'])
+#             elif name == "Is conflict avoided": val = container[i]['is_conflict_avoided']
+#             row_str += f"| {str(val)[:20]:<{col_width}}"
+#         print(row_str)
+    
+#     i+=1
+        
+
+#     print("=" * len(header_row) + "\n")
     
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from config import Config , summary_table
-conf1 = Config(5,8,6,3)
-conf2 = Config(1,3,9,8)
+config1 = Config(5,8,6,3)
+config2 = Config(1,2,2,1)
+config3 = Config(-1,15,99,8)
+config4 = Config(10,8,2,5)
+
+
 summary_table()
+
