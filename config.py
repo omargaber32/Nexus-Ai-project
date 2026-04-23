60e949fc735f109770dfd1f317e2eddf20aea439
ammar mahmoud <Fcds.AmmarMahmoud260169@alexu.edu.eg>
2026-04-23 15:05:16 +0200

    end of summary table

--- config.py
+++ config.py
@@ -74,5 +74,43 @@ class Config:
         print("="*70 + "\n")
     
     def summary_table(container):
-        return
+        #Side titles
+        row_names = [
+            "Path of A",
+            "Path of B",
+            "Deviation of A",
+            "Deviation of B",
+            "Len(Nodes evaluated alphabeta)",
+            "Is conflict avoided"
+        ]
+        #Count the elements which add to each config in the container
+        num_attributes = 8
+        col_width = 25
+        header_width = 30
+        # Calculate the number of the configs which in the container
+        num_configs = len(container) // num_attributes
+        # Print the header
+        header_row = f"{'Attribute':<{header_width}}"
+        for i in range(num_configs):
+            header_row += f"| {'Config ' + str(i+1):<{col_width}}"
+        print("\n" + "=" * len(header_row))
+        print(header_row)
+        print("-" * len(header_row))
+        # print rows via pull the values with index
+        for i, name in enumerate(row_names):
+            row_str = f"{name:<{header_width}}"
+            for c_idx in range(num_configs):
+                start_pos = c_idx * num_attributes
+        #Determine the location of the value based on its order in results
+        if name == "Path of A": val = container[start_pos + 1]
+        elif name == "Path of B": val = container[start_pos + 2]
+        elif name == "Deviation of A": val = container[start_pos + 8]
+        elif name == "Deviation of B": val = container[start_pos + 7]
+        elif name == "Len(Nodes evaluated alphabeta)": val = len(container[start_pos + 4])
+        elif name == "Is conflict avoided": val = container[start_pos + 3]
+        
+        row_str += f"| {str(val)[:20]:<{col_width}}"
+        print(row_str)
+
+        print("=" * len(header_row) + "\n")
     
